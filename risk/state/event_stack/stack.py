f4746ae3f296f9d15544fa2be80fc3f38ff289b5
Adam Peter Banham <n7176546@qut.edu.au>
2025-10-19 09:40:34 +1030

    feat: added stacks

--- risk/state/event_stack/stack.py
+++ risk/state/event_stack/stack.py
@@ -3,21 +3,74 @@ This module describes the event stack data structure used
 to manage simulation events.
 """
 
-from typing import Union
+from typing import Union, Dict, List
+from uuid import uuid5, UUID
+
+EVENT_NAMESPACE = UUID("83565e68-4400-496e-a9fe-932f80bcf803")
+LEVEL_NAMESPACE = UUID("38c0f2c1-6ef3-4d4b-8845-7d2a378b3a88")
+STACK_NAMESPACE = UUID("7557cad3-83c5-429a-ba3c-20cae6623b45")
+
+class EventContext:
+    """
+    A base class for holding an immutable context dictionary.
+    """
+
+    def __init__(self, context:Dict[str,object]=None):
+        if context is not None:
+            for key,val in context.items():
+                setattr(self, key, val)
+        self._lock = 1
+
+    def _make_dict(self) -> Dict[str,object]:
+        return dict(
+            (attr, getattr(self, attr))
+            for attr
+            in self.__dict__
+            if attr != "_lock"
+        )
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+    
+    def __setattr__(self, name, value):
+        if hasattr(self, "_lock"):
+            raise TypeError("Contexts are immutable!")
+        else:
+            super().__setattr__(name, value)
+
+    def __repr__(self):
+        return repr(self._make_dict())
 
 class Event:
     """
     A base class for events in the simulation.
     """
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, context:Dict[str,object]=None):
         self.name = name
+        self.context = EventContext(context) if context is not None else EventContext()
+        self.id = uuid5(EVENT_NAMESPACE, str(self))
+        self._lock = 1
 
     def __str__(self) -> str:
-        return f"Event: {self.name}"
+        return f"Event: {self.name}, Context: {self.context}"
 
     def __repr__(self) -> str:
-        return f"Event({repr(self.name)})"
+        return f"Event({repr(self.name)},{repr(self.context)})"
+    
+    def __hash__(self):
+        return hash(self.id)
+    
+    def __eq__(self, value):
+        if isinstance(value, Event):
+            return self.id == value.id 
+        return False
+    
+    def __setattr__(self, name, value):
+        if hasattr(self, '_lock'):
+            raise TypeError("Events are immutable!")
+        else:
+            super().__setattr__(name, value)
 
 class Level:
     """
@@ -26,60 +79,144 @@ class Level:
 
     def __init__(self, name: str):
         self.name = name
+        self.id = uuid5(LEVEL_NAMESPACE, str(self))
+        self._lock = 1
 
     def __str__(self) -> str:
         return f"Level: {self.name}"
 
     def __repr__(self) -> str:
         return f"Level({repr(self.name)})"
+    
+    def __setattr__(self, name, value):
+        if hasattr(self, "_lock"):
+            raise TypeError("Levels are immutable!")
+        super().__setattr__(name, value)
+
+    def __eq__(self, value):
+        if isinstance(value, Level):
+            return self.id == value.id 
+        return False
+    
+    def __hash__(self):
+        return hash(self.id)
+    
+
+class EventStackInfo:
+    current_level:Union[Level, None]=None
+    depth:int=0
+
+    def __init__(self):
+        self.current_level = None 
+        self.depth = 0
 
 class EventStack:
     """
     A stack to manage simulation events.
     """
 
-    def __init__(self):
+    def __init__(self, name:str, layers:List[Union[Event, Level]] = None):
         self.stack = []
+        self.name = name 
+        self.id = uuid5(STACK_NAMESPACE, name)
+        self._info = EventStackInfo()
+        if layers:
+            for layer in layers:
+                self.push(layer)
+        self._lock = 1
 
-    def push(self, event:Union[Level, Event]) -> None:
+    def push(self, element:Union[Level, Event]) -> None:
         """
         Push an event onto the stack.
         """
-        self.stack.append(event)
+        if isinstance(element, Level):
+            self._info.depth += 1
+            self._info.current_level = element
+        self.stack.append(element)
 
     def pop(self) -> Union[None, Level, Event]:
         """
         Pop an event off the stack.
         """
-        if self.is_empty():
+        if self.is_empty:
             return None
+        if isinstance(self.peek(), Level):
+            self._info.depth -= 1
+            if (self.depth > 0):
+                self._info.current_level = self._find_next_level(self.stack[:-1])
+            else:
+                self._info.current_level = None
+
         return self.stack.pop()
 
     def peek(self) -> Union[None, Level, Event]:
         """
         Peek at the top event on the stack without removing it.
         """
-        if self.is_empty():
+        if self.is_empty:
             return None
         return self.stack[-1]
     
-    def current_level(self) -> Union[None, Level]:
+    def _find_next_level(self, stack) -> Union[None, Level]:
         """
-        Get the current level from the top of the stack.
+        Walks the stack to find the closest level.
         """
-        for item in reversed(self.stack):
+        for item in reversed(stack):
             if isinstance(item, Level):
                 return item
         return None
-
+    
+    @property
+    def current_level(self) -> Union[None, Level]:
+        """
+        Gets the current level from the top of the stack.
+        """
+        return self._info.current_level
+    
+    @property    
+    def depth(self) -> int:
+        """
+        Gets the depth of the current stack (i.e. how many levels deep is 
+        the current top event).
+        """
+        return self._info.depth
+    
+    @property
     def is_empty(self) -> bool:
         """
         Check if the stack is empty.
         """
         return len(self.stack) == 0
 
+    @property
     def size(self) -> int:
         """
         Get the current size of the stack.
         """
         return len(self.stack)
+    
+    def substack(self, layers:int) -> 'EventStack':
+        """
+        From the bottom up create a substack of the current stack with up
+        to `layers` in it.
+        """
+
+        return EventStack(self.name + f"-sub-{layers}",
+            layers=self.stack[:layers]                
+        )
+
+    def __len__(self):
+        return self.size
+    
+    def __setattr__(self, name, value):
+        if hasattr(self, "_lock"):
+            raise TypeError("Stacks are immutable, outside of modifying the stack")
+        super().__setattr__(name, value)
+
+    def __hash__(self):
+        return hash(self.id)
+    
+    def __eq__(self, value):
+        if isinstance(value, EventStack):
+            return self.id == value.id
+        return False 
--- tests/state/events/test_stacks.py
+++ tests/state/events/test_stacks.py
@@ -1,17 +1,264 @@
 import unittest
-from unittest.mock import Mock
 
 from risk.state.event_stack import Event, Level, EventStack
 
 class TestEvents(unittest.TestCase):
 
-    pass 
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_create(self):
+        ev = Event("attack")
+        ev2 = Event("defend")
+        ev3 = Event("attack")
+        ev4 = Event("attack", { 'p1' : 1}) 
+
+        with self.assertRaises(TypeError):
+            ev.id = "foo"
+        with self.assertRaises(AttributeError):
+            ev.context.foo
+        with self.assertRaises(TypeError):
+            ev.context.bar = 2
+
+    def test_eq(self):
+        ev = Event("attack")
+        ev2 = Event("defend")
+        ev3 = Event("attack")
+        ev4 = Event("attack", { 'p1' : 1}) 
+
+        self.assertTrue(ev != ev2)
+        self.assertTrue(ev == ev)
+        self.assertTrue(ev == ev3)
+        self.assertTrue(ev4 != ev)
+        self.assertTrue(ev4 != ev2)
+        self.assertTrue(ev4 != ev3)
+        self.assertTrue(ev4.context.p1 == 1)
+
+    def test_str(self):
+        ev = Event("attack")
+        self.assertEqual(str(ev), "Event: attack, Context: {}")
+
+        ev = Event("attack", dict(launch="now"))
+        self.assertEqual(str(ev), f"Event: attack, Context: {str({'launch': 'now'})}")
+
+    def test_hash(self):
+        ev = Event("attack")
+        ev2 = Event("defend")
+
+        holder = {
+            ev : "foo",
+            ev2 : "bar"
+        }
+
+        self.assertTrue(ev in holder)
+        self.assertTrue(ev2 in holder)
+        self.assertEqual(holder[ev], "foo")
+        self.assertEqual(holder[ev2], "bar")
+
+    def test_repr(self):
+        ev = Event("attack")
+        ev4 = Event("defend", { 'p1' : 1}) 
+
+        self.assertEqual(eval(repr(ev)), ev)
+        self.assertEqual(eval(repr(ev4)), ev4)
+    
 
 class TestLevel(unittest.TestCase):
 
-    pass
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_create(self):
+        
+        turn_one = Level("Turn 1")
+
+        with self.assertRaises(TypeError):
+            turn_one.id = "foo"
+        with self.assertRaises(TypeError):
+            turn_one["bar"] = "baz"
+
+    def test_eq(self):
+
+        turn_one = Level("Turn 1")
+        turn_one_b = Level("Turn 1")
+
+        turn_two = Level("Turn 2") 
+
+        self.assertNotEqual(turn_one, turn_two)
+        self.assertEqual(turn_one, turn_one_b)
+
+    def test_repr(self):
+
+        turn_one = Level("Turn 1")
+
+        self.assertEqual(turn_one, eval(repr(turn_one)))
+
+    def test_hash(self):
+
+        turn_one = Level("Turn 1")
+        turn_two = Level("Turn 2")
+
+        holder = {
+            turn_one : "foo",
+            turn_two : "bar"
+        }
+
+        self.assertTrue(turn_one in holder)
+        self.assertTrue(turn_two in holder)
+        self.assertEqual(holder[turn_one], "foo")
+        self.assertEqual(holder[turn_two], "bar")
+
+    def test_collide(self):
+
+        turn_one = Level("Turn 1")
+        event = Event("Turn 1")
+
+        self.assertNotEqual(turn_one, event)
+        self.assertNotEqual(turn_one.id, event.id)
 
 class TestEventStack(unittest.TestCase):
-    
-    pass 
+
+    def test_create(self):
+        stack = EventStack("Game Loop")
+
+        with self.assertRaises(TypeError):
+            stack.foo = "bar"
+        with self.assertRaises(TypeError):
+            stack['baz'] = 2
+        with self.assertRaises(TypeError):
+            stack.id = 2
+
+    def test_push(self):
+        ev = Event("event 1")
+        ev2 = Event("event 2")
+        ev3 = Event("event 3")
+
+        stack = EventStack("loop")
+
+        self.assertEqual(len(stack), 0)
+        stack.push(ev)
+        self.assertEqual(len(stack), 1)
+        stack.push(ev2)
+        self.assertEqual(len(stack), 2)
+        stack.push(ev3)
+        self.assertEqual(len(stack), 3)
+        self.assertEqual(stack.peek(), ev3)
+
+    def test_pop(self):
+        ev = Event("event 1")
+        ev2 = Event("event 2")
+        ev3 = Event("event 3")
+
+        stack = EventStack("loop")
+        stack.push(ev)
+        stack.push(ev2)
+        stack.push(ev3)
+
+        self.assertEqual(len(stack), 3)
+        out = stack.pop()
+        self.assertEqual(out, ev3)
+        self.assertEqual(len(stack), 2)
+        out = stack.pop()
+        self.assertEqual(out, ev2)
+        self.assertEqual(len(stack), 1)
+        out = stack.pop()
+        self.assertEqual(out, ev)
+        self.assertEqual(len(stack), 0)
+        out = stack.pop()
+        self.assertEqual(out, None)
+
+    def test_level(self):
+        ev = Event("event 1")
+        level_one = Level("Turn 1")
+        ev2 = Event("event 2")
+        ev3 = Event("event 3")
+        level_two = Level("Turn 2")
+        ev4 = Event("event 4")
+
+        stack = EventStack("loop")
+        out = stack.current_level
+        self.assertEqual(out, None)
+
+        stack.push(ev)
+        stack.push(level_one)
+        out = stack.current_level
+        self.assertEqual(out, level_one)
+
+        stack.push(ev2)
+        stack.push(ev3)
+        stack.push(level_two)
+        out = stack.current_level
+        self.assertEqual(out, level_two)
+
+        stack.push(ev4)
+        stack.pop()
+        stack.pop()
+        out = stack.current_level
+        self.assertEqual(out, level_one)
+
+    def test_depth(self):
+        ev = Event("event 1")
+        level_one = Level("Turn 1")
+        ev2 = Event("event 2")
+        ev3 = Event("event 3")
+        level_two = Level("Turn 2")
+
+        stack = EventStack("loop")
+        self.assertEqual(stack.depth, 0)
+
+        stack.push(ev)
+        stack.push(level_one)
+        self.assertEqual(stack.depth, 1)
+
+        stack.push(ev2)
+        stack.push(ev3)
+        stack.push(level_two)
+        self.assertEqual(stack.depth, 2)
+
+        stack.pop()
+        self.assertEqual(stack.depth, 1)
+
+        stack.pop()
+        stack.pop()
+        stack.pop()
+        self.assertEqual(stack.depth, 0)
+
+    def test_substack(self):
+        ev = Event("event 1")
+        level_one = Level("Turn 1")
+        ev2 = Event("event 2")
+        ev3 = Event("event 3")
+        level_two = Level("Turn 2")
+
+        stack = EventStack("loop")
+
+        stack.push(ev)
+        stack.push(level_one)
+        new_stack = stack.substack(stack.size)
+        self.assertEqual(new_stack.current_level, stack.current_level)
+        self.assertNotEqual(new_stack, stack)
+        self.assertNotEqual(stack.substack(-1).current_level, stack.current_level)
+
+        stack.push(ev2)
+        stack.push(ev3)
+        stack.push(level_two)
+        new_stack = stack.substack(stack.size)
+        self.assertEqual(new_stack.current_level, stack.current_level)
+        self.assertNotEqual(new_stack, stack)
+        self.assertNotEqual(stack.substack(-1).current_level, stack.current_level)
+        self.assertEqual(stack.substack(-1).current_level, level_one)
+
+    def test_collide(self):
+        event_one = Event("turn one")
+        level_one = Level("turn one")
+        stack = EventStack("turn one")
+
+        self.assertNotEqual(event_one.id, stack.id)
+        self.assertNotEqual(level_one.id, stack.id) 
 
