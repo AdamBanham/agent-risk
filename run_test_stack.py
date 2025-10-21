41bc0908ebb844242f0e1fb7e84a411e1d01f52a
Dr Adam Banham <adam.banham@outlook.com.au>
2025-10-21 18:40:34 +1030

    chore: testing for sim controller

---
+++ run_test_stack.py
@@ -0,0 +1,132 @@
+
+import sys
+from risk.engine.controller import RiskSimulationController
+from risk.engine.base import RecordStackEngine, Engine
+from risk.state.event_stack import GameEvent
+from risk.state.game_state import GameState
+
+from time import time, sleep
+
+state = GameState.create_new_game(
+    regions=10,
+    num_players=2,
+    starting_armies=20
+)
+state.initialise()
+
+from risk.state.event_stack import PlacementPhase, TroopPlacementEvent
+class MockPlacements(Engine):
+    """
+    Mock engine for handling troop placements.
+    """
+
+    allowed_elements = [
+        PlacementPhase
+    ]
+
+    def __init__(self):
+        super().__init__("MockPlacementsEngine")
+    
+    def process(self, game_state: GameState, element: PlacementPhase) -> None:
+        # For each player, place 1 troop in the first territory they own
+        placement = []
+
+        territories = list(game_state.get_territories_owned_by(
+            game_state.current_player_id 
+        ))
+        other_territories = list(game_state.get_territories_owned_by(
+            (game_state.current_player_id + 1) % game_state.num_players
+        ))
+
+        placer = territories[0]
+        other = other_territories[0]
+
+        # simulate adding troops for non current player
+        placement.append(
+            TroopPlacementEvent(
+                turn=game_state.total_turns,
+                player=game_state.current_player_id,
+                territory=other.id,
+                num_troops=1
+            )
+        )
+        # simulate adding troops for non current player
+        placement.append(
+            TroopPlacementEvent(
+                turn=game_state.total_turns,
+                player=(game_state.current_player_id + 1) % game_state.num_players,
+                territory=placer.id,
+                num_troops=3
+            )
+        )
+        # simulate undo by placing another troop placement
+        placement.append(
+            TroopPlacementEvent(
+                turn=game_state.total_turns,
+                player=game_state.current_player_id,
+                territory=placer.id,
+                num_troops= -1
+            )
+        )
+        # simulate adding troops
+        placement.append(
+            TroopPlacementEvent(
+                turn=game_state.total_turns,
+                player=game_state.current_player_id,
+                territory=placer.id,
+                num_troops=2
+            )
+        )
+
+
+        return placement
+
+recorder = RecordStackEngine()
+engine = RiskSimulationController(
+    game_state=state
+)
+engine.add_engine(MockPlacements())
+engine.add_engine(recorder)
+
+engine.event_stack.push(
+    GameEvent()
+)
+
+delay = 1
+
+original_stdout = sys.stdout
+try :
+    with open("test.out", "w") as f:
+        # Redirect stdout to the file
+        sys.stdout = f
+        try:
+            f.flush()
+            print("Initial Event Stack:")
+            print(str(engine.event_stack))
+            print()
+
+            # Process the event stack until empty
+            while not engine.event_stack.is_empty:
+                touched = engine.step()
+                print(f"Engine processed something: {touched}")
+                print("Current Event Stack:")
+                print(str(engine.event_stack))
+                print()
+                sleep(delay)
+
+                original_stdout.write('\033c')
+                original_stdout.write(str(recorder.stack) + "\n")
+        finally:
+            # Restore original stdout
+            sys.stdout = original_stdout
+except KeyboardInterrupt:
+    sys.stdout = original_stdout
+    f.close()
+    print("Test interrupted by user.")
+    print(recorder.stack)
+    with open("test.stack", "w") as f:
+        f.write(str(recorder.stack))
+    with open("test.state", "w") as f:
+        f.write(str(state))
+    sys.exit(1)
+
