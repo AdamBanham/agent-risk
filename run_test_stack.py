83f676d783bfbe2159bf845656cef90b5c2b2fdf
Dr Adam Banham <adam.banham@outlook.com.au>
2025-10-22 11:03:50 +1030

    testing: added mock for attack requests

--- run_test_stack.py
+++ run_test_stack.py
@@ -1,4 +1,5 @@
 
+import random
 import sys
 from risk.engine.controller import RiskSimulationController
 from risk.engine.base import RecordStackEngine, Engine
@@ -78,21 +79,121 @@ class MockPlacements(Engine):
             )
         )
 
+        return [random.choice(placement)]
 
-        return placement
+from risk.state.event_stack import (
+    AttackPhase,
+    AttackOnTerritoryEvent,
+)
+
+class MockFights(Engine):
+    """
+    Mock engine for placing fights on the stack.
+    """
+
+    allowed_elements = [
+        AttackPhase
+    ]
+
+    def __init__(self):
+        super().__init__("MockFightsEngine")
+    
+    def process(self, 
+                game_state: GameState, 
+                element: AttackPhase) -> None:
+        # For each player, place 1 troop in the first territory they own
+        placement = []
+
+        territories = list(game_state.get_territories_owned_by(
+            game_state.current_player_id 
+        ))
+        other_territories = list(game_state.get_territories_owned_by(
+            (game_state.current_player_id + 1) % game_state.num_players
+        ))
+
+        placer = random.choice(territories)
+        other = random.choice(other_territories)
+
+        # simulate an attack
+        for adjacent in placer.adjacent_territories:
+            placement.append(
+                AttackOnTerritoryEvent(
+                    turn=game_state.total_turns,
+                    player=game_state.current_player_id,
+                    from_territory=placer.id,
+                    to_territory=adjacent.id,
+                    attacking_troops=1
+                )
+            )
+
+        # add some fail cases
+        fails = []
+        ## not your turn
+        fails.append(
+            AttackOnTerritoryEvent(
+                turn=game_state.total_turns,
+                player=(game_state.current_player_id + 1) % game_state.num_players, 
+                from_territory=placer.id,
+                to_territory=adjacent.id,
+                attacking_troops=1
+            )
+        )
+        ## not your territory
+        fails.append(
+            AttackOnTerritoryEvent(
+                turn=game_state.total_turns,
+                player=game_state.current_player_id,
+                from_territory=other.id,
+                to_territory=adjacent.id,
+                attacking_troops=1
+            )
+        )
+        ## no adjacent territory
+        fails.append(
+            AttackOnTerritoryEvent(
+                turn=game_state.total_turns,
+                player=game_state.current_player_id,
+                from_territory=placer.id,
+                to_territory=[t.id for t in game_state.territories.values() if t.id not in [adj.id for adj in placer.adjacent_territories]][0],
+                attacking_troops=1
+            )
+        )
+        ## zero attacking troops
+        fails.append(
+            AttackOnTerritoryEvent(
+                turn=game_state.total_turns,
+                player=game_state.current_player_id,
+                from_territory=placer.id,
+                to_territory=adjacent.id,
+                attacking_troops=0
+            )
+        )
+        ## too many attacking troops
+        fails.append(
+            AttackOnTerritoryEvent(
+                turn=game_state.total_turns,
+                player=game_state.current_player_id,
+                from_territory=placer.id,
+                to_territory=adjacent.id,
+                attacking_troops=placer.armies + 5
+            )
+        )
+
+        return placement + [random.choice(fails)]
 
 recorder = RecordStackEngine()
 engine = RiskSimulationController(
     game_state=state
 )
 engine.add_engine(MockPlacements())
+engine.add_engine(MockFights())
 engine.add_engine(recorder)
 
 engine.event_stack.push(
     GameEvent()
 )
 
-delay = 1
+delay = 0.5
 
 original_stdout = sys.stdout
 try :
@@ -122,11 +223,12 @@ try :
 except KeyboardInterrupt:
     sys.stdout = original_stdout
     f.close()
+    print('\033c')
     print("Test interrupted by user.")
     print(recorder.stack)
     with open("test.stack", "w") as f:
         f.write(str(recorder.stack))
     with open("test.state", "w") as f:
-        f.write(str(state))
+        f.write(repr(state))
     sys.exit(1)
 
