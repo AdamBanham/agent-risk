515a7433fbd88a2b74cd39840b2237f772e9046e
Dr Adam Banham <adam.banham@outlook.com.au>
2025-12-10 16:41:40 +1030

    chore: add player arg to runner

--- run_sim_from_test.py
+++ run_sim_from_test.py
@@ -7,7 +7,9 @@ import argparse
 def parse_args():
 
     parser = argparse.ArgumentParser(
-        description="Run a simulation from a saved test game state."
+        description="Run a simulation from a saved test game state." \
+        "Ensures that player one is a human player, and all other players are AI agents." \
+        "Setting --player will set that player as the human player.",
     )
 
     parser.add_argument(
@@ -22,6 +24,14 @@ def parse_args():
         action="store_true",
         help="Enable debug logging.",
     )
+
+    parser.add_argument(
+        "--player",
+        type=int,
+        default=0,
+        help="ID of the human player (default: 1).",
+    )
+
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -36,9 +46,12 @@ if __name__ == "__main__":
     test_state.initialise(False)
     test_state.update_player_statistics()
 
+    if args.player >= test_state.num_players or args.player < 0:
+        raise ValueError(f"Player ID {args.player} is out of range for this game.")
+
     game_loop = create_ai_game(
         ai_player_ids=[
-            player.id for player in test_state.players.values() if player.id != 0
+            player.id for player in test_state.players.values() if player.id != args.player
         ],
         play_from_state=test_state,
     )
